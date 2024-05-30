4439cbbc09dcfb63d02dee61d6f2cf1437eb4f26
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2024-05-30 11:48:38 -0500

    More work on camera controler

--- player/camera_controller.gd
+++ player/camera_controller.gd
@@ -5,6 +5,7 @@ const CAMERA_MOVEMENT_SPEED:float = 0.1
 @onready var x_rot = $"Y_Rot/X_Rot"
 @onready var y_rot = $Y_Rot
 @onready var Camera = $Y_Rot/X_Rot/Camera3D
+@onready var Position_Target = $Position_Target
 
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,11 +14,24 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.is_action_pressed("middle-mouse"):
 		if Input.is_action_pressed("shift"):
+			# get the angle of the mouse movement relative to the screen
+			# then apply that to the camera-controler node location, with the angle paralel to the camera
+			var movement_vector:Vector2 = Vector2(event.relative.x, event.relative.y)
+			var distance = sqrt((event.relative.x ** 2) + (event.relative.y ** 2))
+			#print(rad_to_deg(movement_vector.angle()))
+			print(distance)
+
+			Position_Target.position = y_rot.position
+			Position_Target.global_rotation = Camera.global_rotation
+			Position_Target.rotation_degrees.x = rad_to_deg(movement_vector.angle())
+			#Position_Target.position.z += distance * 0.01
+
+			#y_rot.position = Position_Target.global_position
 			y_rot.position.x += float(-event.relative.y) * CAMERA_MOVEMENT_SPEED
 			y_rot.position.z += float(-event.relative.x) * CAMERA_MOVEMENT_SPEED
 		else:
-			x_rot.global_rotation_degrees.x += float(-event.relative.y) * CAMERA_ROT_SPEED
 			y_rot.global_rotation_degrees.y += float(-event.relative.x) * CAMERA_ROT_SPEED
+			x_rot.global_rotation_degrees.x = clamp(x_rot.global_rotation_degrees.x + float(-event.relative.y) * CAMERA_ROT_SPEED, -80, 50)
 
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
