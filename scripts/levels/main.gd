8de024de3e87c9a9c014548f91695b8814185dd9
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2024-06-20 12:26:16 -0500

    Added lights, updated enviroment

--- scripts/levels/main.gd
+++ scripts/levels/main.gd
@@ -26,6 +26,6 @@ func _ready():
 					add_child(level, true)
 
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
