f009c5cf3e378a3e6a448c5914e980e237140de8
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2024-06-20 00:00:41 -0500

    It's still midnight

--- player/player.gd
+++ player/player.gd
@@ -3,9 +3,12 @@ extends CharacterBody3D
 const CAMERA_ROT_SPEED = 0.2
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+
 @onready var camera = $Camera3D
 var xrot = 0.0
 var yrot = 0.0
+
+var crouched:bool = false
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -17,6 +20,13 @@ func _input(event):
 		yrot += float(-event.relative.x) * CAMERA_ROT_SPEED
 		camera.rotation_degrees.x = clamp(xrot, -80, 89.9)
 		self.rotation_degrees.y = yrot 
+	if Input.is_action_just_pressed("Crouch"):
+		if not crouched:
+			$AnimationPlayer.play("Crouch")
+			crouched = true
+		else:
+			$AnimationPlayer.play_backwards("Crouch")
+			crouched = false
 
 
 func _physics_process(delta: float) -> void:
