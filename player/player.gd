c800235a682775e4aa7ee12dbf6adc9d7bc650c6
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2024-06-22 17:24:41 -0500

    Removed debug line

--- player/player.gd
+++ player/player.gd
@@ -44,7 +44,6 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		print(velocity.y)
 		if abs(velocity.y) > top_fall_speed:
 			top_fall_speed = abs(velocity.y)
 	else:
