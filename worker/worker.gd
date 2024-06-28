146f4c9157ffadf2a4e2a91537b8cf869cb9e83d
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2024-06-28 15:40:45 -0500

    More work on workers that don't work

--- worker/worker.gd
+++ worker/worker.gd
@@ -1,18 +1,28 @@
 extends Node3D
 
 var mouse_inside:bool = false
+var waiting_orders: bool = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Area3D/MeshInstance3D.visible = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if mouse_inside and not Input.is_action_pressed("middle-mouse"):
+	if mouse_inside and not Input.is_action_pressed("middle-mouse") and not waiting_orders:
 		$Area3D/MeshInstance3D.visible = true
-	else:
+	elif not waiting_orders:
 		$Area3D/MeshInstance3D.visible = false
+	if waiting_orders and not $Area3D/AnimationPlayer.is_playing():
+		$Area3D/AnimationPlayer.play("ready")
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("left-mouse") and mouse_inside:
+		waiting_orders = true
+		$Control.visible = true
+	if waiting_orders and Input.is_action_just_pressed("escape"):
+		waiting_orders =  false
+		$Control.visible = false
 
 
 func _on_mouse_entered() -> void:
@@ -21,3 +31,9 @@ func _on_mouse_entered() -> void:
 
 func _on_mouse_exited() -> void:
 	mouse_inside = false
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	print("finished")
+	if anim_name == "ready":
+		$Area3D/AnimationPlayer.play("waiting")
